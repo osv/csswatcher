417d53b83fd8da379b37cb02164b0eda788a007b
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-01-16 22:57:11 +0200

    minor fix

--- lib/CSS/Watcher.pm
+++ lib/CSS/Watcher.pm
@@ -15,7 +15,7 @@ use CSS::Watcher::Parser;
 use CSS::Watcher::ParserLess;
 use CSS::Watcher::Monitor;
 
-our $VERSION = '0.3.3';
+our $VERSION = '0.3.4';
 
 use constant DEFAULT_HTML_STUFF_DIR => '~/.emacs.d/ac-html-csswatcher/completion/';
 
